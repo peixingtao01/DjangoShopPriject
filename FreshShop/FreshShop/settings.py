606f659bd0255e5cc125af57a77250024a6df45a
pei_youtube <pei_youtube>
2019-07-22 14:04:44 +0800

    卖家端

--- FreshShop/FreshShop/settings.py
+++ FreshShop/FreshShop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#l68(4dw#u@ekwup%*e229#gmr!h738+msmg5i!nefdvkt54%w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ShopFresh1'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'static')
+
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # DjangoShopPriject
 Django的一个电商项目
 2019.7.22创建
+7.22{
+创建了sqlite数据库，卖家端
+收集了静态文件，配置了setting
+}
